2cfd7ab0ee1833b27689e45bfd847a5bebf6bed1
MR.REX <mrrex.main@gmail.com>
2025-01-16 19:06:30 +0300

    Now MavenArtifact has checksum and implements StorableEntity interface

--- src/main/java/ru/mrrex/betterium/entities/MavenArtifact.java
+++ src/main/java/ru/mrrex/betterium/entities/MavenArtifact.java
@@ -2,12 +2,14 @@ package ru.mrrex.betterium.entities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Path;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import ru.mrrex.betterium.entities.interfaces.DownloadableEntity;
+import ru.mrrex.betterium.entities.interfaces.StorableEntity;
 
-public class MavenArtifact implements DownloadableEntity {
+public class MavenArtifact implements DownloadableEntity, StorableEntity {
 
     @JsonProperty("group_id")
     private String groupId;
@@ -16,6 +18,7 @@ public class MavenArtifact implements DownloadableEntity {
     private String artifactId;
 
     private String version;
+    private String checksum;
 
     public String getGroupId() {
         return groupId;
@@ -41,6 +44,14 @@ public class MavenArtifact implements DownloadableEntity {
         this.version = version;
     }
 
+    public String getChecksum() {
+        return checksum;
+    }
+
+    public void setChecksum(String checksum) {
+        this.checksum = checksum;
+    }
+
     @Override
     public URL getUrl() {
         String remoteJarPath = "https://repo1.maven.org/maven2/%s/%s/%s/%s-%s.jar".formatted(
@@ -56,6 +67,11 @@ public class MavenArtifact implements DownloadableEntity {
         } catch (MalformedURLException exception) {
             throw new RuntimeException("Failed to generate link to .jar file from the Maven repository", exception);
         }
+    }
+
+    @Override
+    public Path getRelativePath() {
+        return Path.of(groupId, artifactId + "-" + version + ".jar");
     }
 
     @Override
